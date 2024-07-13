d1a48e67cfc8a9b7103ce213ad5ea43e4fffebd8
Dns <102502216+DNS239@users.noreply.github.com>
2024-07-13 05:17:24 -0300

    #

---
+++ Node-Dio/05-carrinho-de compras-da-shopee/src/services/cart.js
@@ -0,0 +1,19 @@
+
+async function addItem(userCart) {
+    
+}
+
+
+async function deletItem(userCart, name) {
+    
+}
+
+
+async function removeItem(userCart, index) {
+    
+}
+
+
+async function calculateTotal(userCart) {
+    
+}
