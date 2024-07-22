36a688c38b526e53da5582a65143d38f916a7f25
Dns <102502216+DNS239@users.noreply.github.com>
2024-07-21 23:52:58 -0300

    #

--- Node-Dio/05-carrinho-de compras-da-shopee/src/index.js
+++ Node-Dio/05-carrinho-de compras-da-shopee/src/index.js
@@ -1,1 +1,27 @@
-const cart = [];
+import * as cartServices from './services/cart.js'
+import createItem from "./services/item.js";
+
+
+const myCart = [];
+const myWhishList = [];
+
+
+console.log("Welcome to the your Shopee Cart!");
+
+
+const item1 = await createItem("hotweels ferrari", 20.99, 1);
+const item2 = await createItem("hotweels lamborghine", 39.99, 3);
+
+
+await cartServices.addItem(myCart, item1);
+await cartServices.addItem(myCart, item2);
+
+await cartServices.removeItem(myCart, 1)
+
+await cartServices.displayCart(myCart);
+
+// await cartServices.deletItem(myCart, item2.name);
+// await cartServices.deletItem(myCart, item1.name);
+
+console.log("Shopee Cart Total is:");
+await cartServices.calculateTotal(myCart);
--- Node-Dio/05-carrinho-de compras-da-shopee/src/services/cart.js
+++ Node-Dio/05-carrinho-de compras-da-shopee/src/services/cart.js
@@ -1,19 +1,51 @@
 
-async function addItem(userCart) {
-    
+async function addItem(userCart, item) {
+    userCart.push(item);
+}
+
+async function calculateTotal(userCart) {
+    const result = userCart.reduce((total, item) => total + item.subtotal(), 0);
+
+    console.log(`\n🎁Total: ${result}`);
 }
 
 
+
 async function deletItem(userCart, name) {
-    
+    const index = userCart.findIndex((item) => item.name === name);
+
+    if (index != -1) {
+        userCart.splice(index, 1)
+    }
 }
 
 
 async function removeItem(userCart, index) {
-    
+
+    const deletIndex = index - 1;
+
+    if (index >= 0 && index < userCart.length) {
+        userCart.splice(deletIndex, 1);
+    }
 }
 
 
-async function calculateTotal(userCart) {
-    
+async function displayCart(userCart) {
+    console.log("\nShoppe Cart list:");
+    userCart.forEach((item, index) => {
+        console.log(`${index + 1}. ${item.name} - R$ ${item.price} | ${item.quantity}x | Subtotal= ${item.subtotal()}`);
+    });
+};
+
+
+
+
+
+
+export {
+    addItem,
+    calculateTotal,
+    deletItem,
+    removeItem,
+    displayCart,
 }
